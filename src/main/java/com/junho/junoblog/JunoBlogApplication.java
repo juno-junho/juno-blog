44f4ab00c8b7b19c0c0e6d70e9741551dbb7c399
juno-junho <ssmm0205@naver.com>
2023-07-14 13:52:44 +0900

    project setting & initial commit

---
+++ src/main/java/com/junho/junoblog/JunoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.junho.junoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunoBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junho/junoblog/JunoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junho.junoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
